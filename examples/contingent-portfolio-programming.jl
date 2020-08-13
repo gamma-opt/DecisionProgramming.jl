91b0f4684dd96d0c4f5d8533bf1400462f0ede95
Jaan Tollander de Balsch <dev@jaantollander.com>
2020-08-13 11:14:47 +0300

    CPP syntax improvements

--- examples/contingent-portfolio-programming.jl
+++ examples/contingent-portfolio-programming.jl
@@ -11,25 +11,23 @@ const CT_states = ["low", "medium", "high"]
 const DA_states = ["0-5 applications", "5-10 applications", "10-15 applications"]
 const CM_states = ["low", "medium", "high"]
 
+S = States([
+    (length(DP_states), [dᴾ]),
+    (length(CT_states), [cᵀ]),
+    (length(DA_states), [dᴬ]),
+    (length(CM_states), [cᴹ]),
+])
 C = Vector{ChanceNode}()
 D = Vector{DecisionNode}()
 V = Vector{ValueNode}()
-
 X = Vector{Probabilities}()
 Y = Vector{Consequences}()
-
-S_j = Vector{State}(undef, 4)
-S_j[dᴾ] = length(DP_states)
-S_j[cᵀ] = length(CT_states)
-S_j[dᴬ] = length(DA_states)
-S_j[cᴹ] = length(CM_states)
-S = States(S_j)
 
 I_DP = Vector{Node}()
 push!(D, DecisionNode(dᴾ, I_DP))
 
 I_CT = [dᴾ]
-X_CT = zeros(S_j[dᴾ], S_j[cᵀ])
+X_CT = zeros(S[dᴾ], S[cᵀ])
 X_CT[1, :] = [1/2, 1/3, 1/6]
 X_CT[2, :] = [1/3, 1/3, 1/3]
 X_CT[3, :] = [1/6, 1/3, 1/2]
@@ -40,7 +38,7 @@ I_DA = [dᴾ, cᵀ]
 push!(D, DecisionNode(dᴬ, I_DA))
 
 I_CM = [cᵀ, dᴬ]
-X_CM = zeros(S_j[cᵀ], S_j[dᴬ], S_j[cᴹ])
+X_CM = zeros(S[cᵀ], S[dᴬ], S[cᴹ])
 X_CM[1, 1, :] = [2/3, 1/4, 1/12]
 X_CM[1, 2, :] = [1/2, 1/3, 1/6]
 X_CM[1, 3, :] = [1/3, 1/3, 1/3]
@@ -73,7 +71,7 @@ Y = Y[s_v]
 P = DefaultPathProbability(C, X)
 
 @info("Defining DecisionModel")
-@time model = DecisionModel(S, D, P; positive_path_utility=false)
+model = DecisionModel(S, D, P; positive_path_utility=false)
 
 @info("Creating problem specific constraints and expressions")
 
@@ -90,27 +88,41 @@ dl_A = [0, 5, 10]
 du_A = [5, 10, 15]
 M = 20
 
-V_A = rand(S_j[cᴹ], n_A).+0.5
+V_A = rand(S[cᴹ], n_A).+0.5
 V_A[1, :] .+= -0.5  # Low market share: less value
 V_A[3, :] .+= 0.5   # High market share: more value
 
-x_T = variables(model, [S_j[dᴾ]...,n_T]; binary=true)
-x_A = variables(model, [S_j[dᴾ]...,S_j[cᵀ]...,S_j[dᴬ]..., n_A]; binary=true)
+x_T = variables(model, [S[dᴾ]...,n_T]; binary=true)
+x_A = variables(model, [S[dᴾ]...,S[cᵀ]...,S[dᴬ]..., n_A]; binary=true)
 z_dP = model[:z][1]
 z_dA = model[:z][2]
 
-@constraint(model, [i=1:3], sum(x_T[i,t] for t in 1:n_T) <= z_dP[i]*n_T)
-@constraint(model, [i=1:3, j=1:3, k=1:3], sum(x_A[i,j,k,a] for a in 1:n_A) <= z_dP[i]*n_A)
-@constraint(model, [i=1:3, j=1:3, k=1:3], sum(x_A[i,j,k,a] for a in 1:n_A) <= z_dA[i,j,k]*n_A)
+@constraint(model, [i=1:3],
+    sum(x_T[i,t] for t in 1:n_T) <= z_dP[i]*n_T)
+@constraint(model, [i=1:3, j=1:3, k=1:3],
+    sum(x_A[i,j,k,a] for a in 1:n_A) <= z_dP[i]*n_A)
+@constraint(model, [i=1:3, j=1:3, k=1:3],
+    sum(x_A[i,j,k,a] for a in 1:n_A) <= z_dA[i,j,k]*n_A)
 
-@constraint(model, [i=1:3], dl_P[i] - (1 - z_dP[i])*M <= sum(x_T[i,t]*O_t[t] for t in 1:n_T))
-@constraint(model, [i=1:3], sum(x_T[i,t]*O_t[t] for t in 1:n_T) <= du_P[i] + (1 - z_dP[i])*M - ε)
-@constraint(model, [i=1:3, j=1:3, k=1:3], dl_A[k] - (1 - z_dA[i,j,k])*M <= sum(x_A[i,j,k,a]*O_a[a] for a in 1:n_A))
-@constraint(model, [i=1:3, j=1:3, k=1:3], sum(x_A[i,j,k,a]*O_a[a] for a in 1:n_A) <= du_A[k] + (1 - z_dA[i,j,k])*M - ε)
+@constraint(model, [i=1:3],
+    dl_P[i] - (1 - z_dP[i])*M <= sum(x_T[i,t]*O_t[t] for t in 1:n_T))
+@constraint(model, [i=1:3],
+    sum(x_T[i,t]*O_t[t] for t in 1:n_T) <= du_P[i] + (1 - z_dP[i])*M - ε)
+@constraint(model, [i=1:3, j=1:3, k=1:3],
+    dl_A[k] - (1 - z_dA[i,j,k])*M <= sum(x_A[i,j,k,a]*O_a[a] for a in 1:n_A))
+@constraint(model, [i=1:3, j=1:3, k=1:3],
+    sum(x_A[i,j,k,a]*O_a[a] for a in 1:n_A) <= du_A[k] + (1 - z_dA[i,j,k])*M - ε)
 
 @info("Creating model objective.")
-U = @expression(model, [s in paths(S)], sum(x_A[s[1:3]..., a]*(V_A[s[4],a] - I_a[a]) for a in 1:n_A) - sum(x_T[s[1],t]*I_t[t] for t in 1:n_T))
-EV = @expression(model, sum(model[:π][s...] * U[s] for s in paths(S)))
+struct PathUtility <: AbstractPathUtility
+    expr
+end
+(U::PathUtility)(s::Path) = value.(U.expr[s])
+
+U = PathUtility(@expression(model, [s = paths(S)],
+    sum(x_A[s[1:3]..., a]*(V_A[s[4],a] - I_a[a]) for a in 1:n_A) -
+    sum(x_T[s[1],t]*I_t[t] for t in 1:n_T)))
+EV = @expression(model, sum(model[:π][s...] * U.expr[s] for s in paths(S)))
 @objective(model, Max, EV)
 
 @info("Starting the optimization process.")
@@ -128,6 +140,11 @@ Z = DecisionStrategy(model, D)
 @info("Printing decision strategy:")
 print_decision_strategy(S, Z)
 
-value.(x_T)
+@info("Computing utility distribution.")
+udist = UtilityDistribution(S, P, U, Z)
 
-value.(x_A)
+@info("Printing utility distribution.")
+print_utility_distribution(udist)
+
+@info("Printing statistics")
+print_statistics(udist)
