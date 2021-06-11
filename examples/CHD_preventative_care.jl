da6acc79bfb8ba55084250753102a68386384608
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2021-06-11 14:08:59 +0300

    Added dummy data and restructured some of the data reading in the code accordingly since the dummy data includes a row for risk level 100%

--- examples/CHD_preventative_care.jl
+++ examples/CHD_preventative_care.jl
@@ -9,9 +9,7 @@ using CSV, DataFrames
 const chosen_risk_level = 13
 
 # Reading tests' technical performance data (dummy data in this case)
-p_data = CSV.read("test_probabilities_data.csv", DataFrame)
-# Risk levels that discretise the scale of risk estimates 0%-100%
-risk_levels = vec(collect(0:0.01:1.0))
+data = CSV.read("CHD_preventative_care_data.csv", DataFrame)
 
 
 
@@ -23,10 +21,10 @@ risk_levels = vec(collect(0:0.01:1.0))
 function update_risk_distribution(prior::Int64, t::Int64)
     if t == 1 # the test is TRS
         # P(TRS = result | sick) = P(TRS_if_sick = result) * P(sick) = P(TRS_if_sick = result) * P(prior_risk)
-        numerators = p_data.TRS_if_sick .* risk_levels[prior]
+        numerators = data.TRS_if_sick .* data.risk_levels[prior]
 
         # P(TRS = result) = P(TRS_if_sick = result) * P(sick) + P(TRS_if_healthy = result) * P(healthy)
-        denominators = p_data.TRS_if_sick .* risk_levels[prior]  + p_data.TRS_if_healthy .* (1 - risk_levels[prior])
+        denominators = data.TRS_if_sick .* data.risk_levels[prior]  + data.TRS_if_healthy .* (1 - data.risk_levels[prior])
 
         posterior_risks = numerators./denominators
 
@@ -41,8 +39,8 @@ function update_risk_distribution(prior::Int64, t::Int64)
 
 
     elseif t == 2 #the test is GRS
-        numerators = (p_data.GRS_if_sick .* risk_levels[prior])
-        denominators = p_data.GRS_if_sick .* risk_levels[prior]  + p_data.GRS_if_healthy .* (1 .- risk_levels[prior])
+        numerators = (data.GRS_if_sick .* data.risk_levels[prior])
+        denominators = data.GRS_if_sick .* data.risk_levels[prior]  + data.GRS_if_healthy .* (1 .- data.risk_levels[prior])
 
         posterior_risks =  numerators./denominators
 
@@ -84,22 +82,22 @@ function state_probabilities(risk_p::Array{Float64}, t::Int64, h::Int64, prior::
 
     # copying the probabilities of the scores for ease of readability
     if h == 1 && t == 1    # CHD and TRS
-        p_scores = p_data.TRS_if_sick
+        p_scores = data.TRS_if_sick
     elseif t ==1    # no CHD and TRS
-        p_scores = p_data.TRS_if_healthy
+        p_scores = data.TRS_if_healthy
     elseif h == 1 && t == 2 # CHD and GRS
-        p_scores = p_data.GRS_if_sick
+        p_scores = data.GRS_if_sick
     else # no CHD and GRS
-        p_scores = p_data.GRS_if_healthy
+        p_scores = data.GRS_if_healthy
     end
 
-    for i = 1:101 #iterating through all risk levels 0%, 1%, ..., 99% in risk_levels
+    for i = 1:101 #iterating through all risk levels 0%, 1%, ..., 99%, 100% in data.risk_levels
         for j = 1:100 #iterates through all risk estimates in risk_p
             #finding all risk estimates risk_p[j] within risk level i
             # risk_level[i] <= risk_p < risk_level[i]
-            if i < 101 && risk_levels[i] <= risk_p[j] && risk_p[j] < risk_levels[i+1]
+            if i < 101 && data.risk_levels[i] <= risk_p[j] && risk_p[j] < data.risk_levels[i+1]
                 state_probabilites[i] += p_scores[j]
-            elseif i == 101 && risk_levels[i] <= risk_p[j] #special case: the highest risk level[101] = 100%
+            elseif i == 101 && data.risk_levels[i] <= risk_p[j] #special case: the highest risk level[101] = 100%
                 state_probabilites[i] += p_scores[j]
             end
         end
@@ -153,7 +151,7 @@ push!(X, Probabilities(R0, X_R0))
 
 I_H = [R0]
 X_H = zeros(S[R0], S[H])
-X_H[:, 1] = risk_levels     # 1 = "CHD"
+X_H[:, 1] = data.risk_levels     # 1 = "CHD"
 X_H[:, 2] = 1 .- X_H[:, 1]  # 2 = "no CHD"
 push!(C, ChanceNode(H, I_H))
 push!(X, Probabilities(H, X_H))
@@ -232,7 +230,6 @@ z = DecisionVariables(model, S, D)
 # Defining forbidden paths to include all those where a test is repeated twice
 forbidden_tests = ForbiddenPath[([T1,T2], Set([(1,1),(2,2),(3,1), (3,2)]))]
 scale_factor = 100
-
 π_s = PathProbabilityVariables(model, z, S, P; hard_lower_bound = true, forbidden_paths = forbidden_tests, probability_scale_factor = scale_factor)
 
 EV = expected_value(model, π_s, U)
