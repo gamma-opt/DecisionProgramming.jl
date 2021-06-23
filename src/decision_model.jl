3c4b8c66a19baa7d4a78eb37ef1d25741360ed25
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2021-06-23 11:12:21 +0300

    Removed flag and tolerance calculation from probability_cut. Using default tolerance in isapprox() now.

--- src/decision_model.jl
+++ src/decision_model.jl
@@ -144,16 +144,11 @@ probability_cut(model, π_s, P)
 ```
 """
 function probability_cut(model::Model, π_s::PathProbabilityVariables, P::AbstractPathProbability; probability_scale_factor::Float64=1.0)
-    # Add the constraints only once
-    ϵ = minimum(P(s) for s in keys(π_s))
-    flag = false
     function probability_cut(cb_data)
-        flag && return
         πsum = sum(callback_value(cb_data, π) for π in values(π_s))
-        if !isapprox(πsum, 1.0 * probability_scale_factor, atol= ϵ * probability_scale_factor)
+        if !isapprox(πsum, 1.0 * probability_scale_factor)
             con = @build_constraint(sum(values(π_s)) == 1.0 * probability_scale_factor)
             MOI.submit(model, MOI.LazyConstraint(cb_data), con)
-            flag = true
         end
     end
     MOI.set(model, MOI.LazyConstraintCallback(), probability_cut)
