0d443936cc424d10d2a205efa686626ff7f6b59a
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2021-07-02 12:42:28 +0300

    Modified the CHD example for the new formulation!

--- examples/CHD_preventative_care.jl
+++ examples/CHD_preventative_care.jl
@@ -256,7 +256,7 @@ z = DecisionVariables(model, S, D)
 # Defining forbidden paths to include all those where a test is repeated twice
 forbidden_tests = ForbiddenPath[([T1,T2], Set([(1,1),(2,2),(3,1), (3,2)]))]
 scale_factor = 1000.0
-π_s = PathProbabilityVariables(model, z, S, P; hard_lower_bound = true, forbidden_paths = forbidden_tests, probability_scale_factor = scale_factor)
+π_s = PathProbabilityVariables(model, z, S, P; probability_cut = false, forbidden_paths = forbidden_tests, probability_scale_factor = scale_factor)
 
 EV = expected_value(model, π_s, U)
 @objective(model, Max, EV)
@@ -265,14 +265,14 @@ EV = expected_value(model, π_s, U)
 @info("Starting the optimization process.")
 optimizer = optimizer_with_attributes(
     () -> Gurobi.Optimizer(Gurobi.Env()),
-    "TimeLimit" => 100,
-    "IntFeasTol"=> 1e-9,
-    "MIPGap" => 1e-6
+    "TimeLimit" => 200,
+    #"IntFeasTol"=> 1e-9,
+    #"MIPGap" => 1e-6,
+    "MIPFocus"=>3
 )
 set_optimizer(model, optimizer)
 
 optimize!(model)
-
 
 @info("Extracting results.")
 Z = DecisionStrategy(z)
