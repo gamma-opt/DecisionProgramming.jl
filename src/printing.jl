8484a9a3208e859e49bb8af04889751bb9b52ed0
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2021-08-13 13:33:56 +0300

    Edited the decision strategy printing function to use InfluenceDiagram

--- src/printing.jl
+++ src/printing.jl
@@ -11,9 +11,9 @@ Print decision strategy.
 print_decision_strategy(S, Z)
 ```
 """
-function print_decision_strategy(S::States, Z::DecisionStrategy)
+function print_decision_strategy(diagram::InfluenceDiagram, Z::DecisionStrategy)
     for (d, Z_j) in zip(Z.D, Z.Z_j)
-        a1 = vec(collect(paths(S[d.I_j])))
+        a1 = vec(collect(paths(diagram.S[d.I_j])))
         a2 = [Z_j(s_I) for s_I in a1]
         labels = fill("States", length(a1))
         df = DataFrame(labels = labels, a1 = a1, a2 = a2)
