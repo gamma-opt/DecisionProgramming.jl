d318cfb48bd1ff141e234d4d2e90cc66184d80b4
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2021-09-16 11:28:21 +0300

    Created wrapper function for StateProbabilities so that it can be also called by node and state indices if wanted. For instance in testing.

--- src/analysis.jl
+++ src/analysis.jl
@@ -154,9 +154,35 @@ struct StateProbabilities
 end
 
 """
+    StateProbabilities(diagram::InfluenceDiagram, Z::DecisionStrategy, node::Node, state::State, prior_probabilities::StateProbabilities)
+
+Associate each node with array of conditional probabilities for each of its states occuring in compatible paths given
+    fixed states and prior probability. Fix node and state using their indices.
+
+# Examples
+```julia
+# Prior probabilities
+julia> prior_probabilities = StateProbabilities(diagram, Z)
+julia> StateProbabilities(diagram, Z, Node(2), State(1), prior_probabilities)
+```
+"""
+function StateProbabilities(diagram::InfluenceDiagram, Z::DecisionStrategy, node::Node, state::State, prior_probabilities::StateProbabilities)
+    prior = prior_probabilities.probs[node][state]
+    fixed = deepcopy(prior_probabilities.fixed)
+
+    push!(fixed, node => state)
+    probs = Dict(i => zeros(diagram.S[i]) for i in 1:length(diagram.S))
+    for s in CompatiblePaths(diagram, Z, fixed), i in 1:length(diagram.S)
+        probs[i][s[i]] += diagram.P(s) / prior
+    end
+    StateProbabilities(probs, fixed)
+end
+
+"""
     StateProbabilities(diagram::InfluenceDiagram, Z::DecisionStrategy, node::Name, state::Name, prior_probabilities::StateProbabilities)
 
-Associate each node with array of conditional probabilities for each of its states occuring in compatible paths given fixed states and prior probability.
+Associate each node with array of conditional probabilities for each of its states occuring in compatible paths given
+    fixed states and prior probability. Fix node and state using their names.
 
 # Examples
 ```julia
@@ -173,16 +199,11 @@ function StateProbabilities(diagram::InfluenceDiagram, Z::DecisionStrategy, node
     node_index = findfirst(j -> j ==node, diagram.Names)
     state_index = findfirst(j -> j == state, diagram.States[node_index])
 
-    prior = prior_probabilities.probs[node_index][state_index]
-    fixed = deepcopy(prior_probabilities.fixed)
-
-    push!(fixed, node_index => state_index)
-    probs = Dict(i => zeros(diagram.S[i]) for i in 1:length(diagram.S))
-    for s in CompatiblePaths(diagram, Z, fixed), i in 1:length(diagram.S)
-        probs[i][s[i]] += diagram.P(s) / prior #TODO double check that this is correct
-    end
-    StateProbabilities(probs, fixed)
+    return StateProbabilities(diagram, Z, Node(node_index), State(state_index), prior_probabilities)
 end
+
+
+
 
 """
     StateProbabilities(diagram::InfluenceDiagram, Z::DecisionStrategy)
