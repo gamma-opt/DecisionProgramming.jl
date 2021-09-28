015f3dd72e8efbdded7ec43add8ecdd81fda8de2
solliolli <olli.herrala@aalto.fi>
2021-09-28 15:37:23 +0300

    Finished updating the CPP example

--- examples/contingent-portfolio-programming.jl
+++ examples/contingent-portfolio-programming.jl
@@ -4,14 +4,6 @@ using DecisionProgramming
 
 Random.seed!(42)
 
-function num_states(diagram::InfluenceDiagram, node::Name)
-    idx = findfirst(isequal(node), diagram.Names)
-    if isnothing(idx)
-        throw(DomainError("Name $node not found in the diagram."))
-    end
-    return diagram.S[idx]
-end
-
 @info("Creating the influence diagram.")
 diagram = InfluenceDiagram()
 
@@ -58,6 +50,12 @@ function variables(model::Model, dims::AbstractVector{Int}; binary::Bool=false)
     return v
 end
 
+# Number of states in each node
+n_DP = num_states(diagram, "DP")
+n_CT = num_states(diagram, "CT")
+n_DA = num_states(diagram, "DA")
+n_CM = num_states(diagram, "CM")
+
 n_T = 5                     # number of technology projects
 n_A = 5                     # number of application projects
 I_t = rand(n_T)*0.1         # costs of technology projects
@@ -65,12 +63,12 @@ O_t = rand(1:3,n_T)         # number of patents for each tech project
 I_a = rand(n_T)*2           # costs of application projects
 O_a = rand(2:4,n_T)         # number of applications for each appl. project
 
-V_A = rand(num_states(diagram, "CM"), n_A).+0.5 # Value of an application
+V_A = rand(n_CM, n_A).+0.5 # Value of an application
 V_A[1, :] .+= -0.5          # Low market share: less value
 V_A[3, :] .+= 0.5           # High market share: more value
 
-x_T = variables(model, [num_states(diagram, "DP"), n_T]; binary=true)
-x_A = variables(model, [num_states(diagram, "DP"), num_states(diagram, "CT"), num_states(diagram, "DA"), n_A]; binary=true)
+x_T = variables(model, [n_DP, n_T]; binary=true)
+x_A = variables(model, [n_DP, n_CT, n_DA, n_A]; binary=true)
 
 M = 20                      # a large constant
 ε = 0.5*minimum([O_t O_a])  # a helper variable, allows using ≤ instead of < in constraints (28b) and (29b)
@@ -80,35 +78,35 @@ q_A = [0, 5, 10, 15]        # limits of the application intervals
 z_dP = z.z[1]
 z_dA = z.z[2]
 
-@constraint(model, [i=1:3],
+@constraint(model, [i=1:n_DP],
     sum(x_T[i,t] for t in 1:n_T) <= z_dP[i]*n_T)            #(25)
-@constraint(model, [i=1:3, j=1:3, k=1:3],
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA],
     sum(x_A[i,j,k,a] for a in 1:n_A) <= z_dP[i]*n_A)        #(26)
-@constraint(model, [i=1:3, j=1:3, k=1:3],
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA],
     sum(x_A[i,j,k,a] for a in 1:n_A) <= z_dA[i,j,k]*n_A)    #(27)
 
 #(28a)
-@constraint(model, [i=1:3],
+@constraint(model, [i=1:n_DP],
     q_P[i] - (1 - z_dP[i])*M <= sum(x_T[i,t]*O_t[t] for t in 1:n_T))
 #(28b)
-@constraint(model, [i=1:3],
+@constraint(model, [i=1:n_DP],
     sum(x_T[i,t]*O_t[t] for t in 1:n_T) <= q_P[i+1] + (1 - z_dP[i])*M - ε)
 #(29a)
-@constraint(model, [i=1:3, j=1:3, k=1:3],
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA],
     q_A[k] - (1 - z_dA[i,j,k])*M <= sum(x_A[i,j,k,a]*O_a[a] for a in 1:n_A))
 #(29b)
-@constraint(model, [i=1:3, j=1:3, k=1:3],
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA],
     sum(x_A[i,j,k,a]*O_a[a] for a in 1:n_A) <= q_A[k+1] + (1 - z_dA[i,j,k])*M - ε)
 
-@constraint(model, [i=1:3, j=1:3, k=1:3], x_A[i,j,k,1] <= x_T[i,1])
-@constraint(model, [i=1:3, j=1:3, k=1:3], x_A[i,j,k,2] <= x_T[i,1])
-@constraint(model, [i=1:3, j=1:3, k=1:3], x_A[i,j,k,2] <= x_T[i,2])
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA], x_A[i,j,k,1] <= x_T[i,1])
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA], x_A[i,j,k,2] <= x_T[i,1])
+@constraint(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA], x_A[i,j,k,2] <= x_T[i,2])
 
 @info("Creating model objective.")
-patent_investment_cost = @expression(model, [i=1:diagram.S[1]], sum(x_T[i, t] * I_t[t] for t in 1:n_T))
-application_investment_cost = @expression(model, [i=1:diagram.S[1], j=1:diagram.S[2], k=1:diagram.S[3]], sum(x_A[i, j, k, a] * I_a[a] for a in 1:n_A))
-application_value = @expression(model, [i=1:diagram.S[1], j=1:diagram.S[2], k=1:diagram.S[3], l=1:diagram.S[4]], sum(x_A[i, j, k, a] * V_A[l, a] for a in 1:n_A))
-@objective(model, Max, sum( sum( diagram.P((i,j,k,l)) * (application_value[i,j,k,l] - application_investment_cost[i,j,k]) for j in State(1):diagram.S[2], k in State(1):diagram.S[3], l in State(1):diagram.S[4] ) - patent_investment_cost[i] for i in State(1):diagram.S[1] ))
+patent_investment_cost = @expression(model, [i=1:n_DP], sum(x_T[i, t] * I_t[t] for t in 1:n_T))
+application_investment_cost = @expression(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA], sum(x_A[i, j, k, a] * I_a[a] for a in 1:n_A))
+application_value = @expression(model, [i=1:n_DP, j=1:n_CT, k=1:n_DA, l=1:n_CM], sum(x_A[i, j, k, a] * V_A[l, a] for a in 1:n_A))
+@objective(model, Max, sum( sum( diagram.P(convert.(State, (i,j,k,l))) * (application_value[i,j,k,l] - application_investment_cost[i,j,k]) for j in 1:n_CT, k in 1:n_DA, l in 1:n_CM ) - patent_investment_cost[i] for i in 1:n_DP ))
 
 
 @info("Starting the optimization process.")
@@ -137,8 +135,8 @@ Base.getindex(U::PathUtility, I::Vararg{State,N}) where N = getindex(U.data, I..
 (U::PathUtility)(s::Path) = value.(U[s...])
 
 path_utility = [@expression(model,
-    sum(x_A[s[1:3]..., a] * (V_A[s[4], a] - I_a[a]) for a in 1:n_A) -
-    sum(x_T[s[1], t] * I_t[t] for t in 1:n_T)) for s in paths(diagram.S)]
+    sum(x_A[s[index_of(diagram, "DP")], s[index_of(diagram, "CT")], s[index_of(diagram, "DA")], a] * (V_A[s[index_of(diagram, "CM")], a] - I_a[a]) for a in 1:n_A) -
+    sum(x_T[s[index_of(diagram, "DP")], t] * I_t[t] for t in 1:n_T)) for s in paths(diagram.S)]
 diagram.U = PathUtility(path_utility)
 
 @info("Computing utility distribution.")
