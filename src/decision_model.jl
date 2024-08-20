8aaa06c87901db152b5ae0152a6a750b022a1d4b
Olli Herrala <43684983+solliolli@users.noreply.github.com>
2024-08-20 10:04:27 +0300

    Revert previous commit

--- src/decision_model.jl
+++ src/decision_model.jl
@@ -1,4 +1,4 @@
-using JuMP, DataStructures
+using JuMP
 
 function decision_variable(model::Model, S::States, d::Node, I_d::Vector{Node}, names::Bool, base_name::String="")
     # Create decision variables.
