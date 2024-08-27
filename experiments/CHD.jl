59c9ea58ce7df41698b3bfa1020b3e6f34f74348
solliolli <olli.herrala@aalto.fi>
2024-08-27 09:34:23 +0300

    New figures

--- experiments/CHD.jl
+++ experiments/CHD.jl
@@ -30,12 +30,13 @@ end
 # plot!([0.001,7200], [0.001,7200], ls=:dash, lc=:black, label=false)
 
 # log-axis, >1s
-# scatter(t1, t2, xlabel="v1.2.0", ylabel="v0.1.0", markershape=:x, label="Solution time (seconds)", xlim=[1,1800], ylim=[1,7200], xaxis=:log, yaxis=:log)
-# plot!([1,7200], [1,7200], ls=:dash, lc=:black, label=false)
+scatter(t1[t2.<7200], t2[t2.<7200], xlabel="v1.2.0", ylabel="v0.1.0", markershape=:x, label="Solution time (seconds)", xlim=[1,7200], ylim=[1,7200], legend=:right, xaxis=:log, yaxis=:log, yticks=[10^k for k in 0:3], xticks=[10^k for k in 0:3])
+scatter!(t1[t2.>=7200], t2[t2.>=7200], markershape=:x, label=false)
+plot!([1,7200], [1,7200], ls=:dash, lc=:black, label=false)
+Plots.pdf("CHD_log")
 
 # "regular" axis
-scatter(t1[t2.<7200], t2[t2.<7200], xlabel="v1.2.0", ylabel="v0.1.0", markershape=:x, label="Solution time (seconds)", xlim=[0,900], ylim=[0,7200], legend=:right)
-scatter!(t1[t2.>=7200], t2[t2.>=7200], markershape=:x, label=false)
+scatter(t2[t2.<7200], t1[t2.<7200], ylabel="v1.2.0", xlabel="v0.1.0", markershape=:x, label="Solution time (seconds)", xlim=[0,7200], ylim=[0,3600], legend=:right, size=(1200,400))
+scatter!(t2[t2.>=7200], t1[t2.>=7200], markershape=:x, label=false)
 plot!([0,7200], [0,7200], ls=:dash, lc=:black, label=false)
-
 Plots.pdf("CHD")
